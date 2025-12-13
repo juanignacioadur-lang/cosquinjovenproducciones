2681d611c192a0b9b7a01353a4abd19f8c5b6039
Juan <juanignacioadur@gmail.com>
2025-12-13 19:55:44 -0300

    Renombrar archivos para arreglar deploy en Cloudflare

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Nuevos nombres de archivos
-import Navbar from "./components/Navigation.jsx";
-import Footer from "./components/PageFooter.jsx";
-import InfoBox from "./components/InfoCard.jsx"; 
+// IMPORTAMOS LOS NUEVOS NOMBRES DE ARCHIVOS
+import Navigation from "./components/Navigation.jsx";
+import PageFooter from "./components/PageFooter.jsx";
+import InfoCard from "./components/InfoCard.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import EventsPage from "./pages/EventsPage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
 
-import Inicio from "./pages/HomePage.jsx";
-import Noticias from "./pages/EventsPage.jsx";
-import Contacto from "./pages/ContactPage.jsx";
-
-import "./App.css"; // Si tienes estilos globales aquí
+// Si tenías un archivo CSS global, mantenlo (ej. import "./App.css")
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      {/* Usamos el componente con el nuevo nombre */}
+      <Navigation />
 
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        {/* Mantenemos las rutas url igual (/informacion, /noticias) pero cargan los nuevos archivos */}
-        <Route path="/informacion" element={<Informacion />} /> 
-        {/* OJO: Si tenías una página "Informacion.jsx", asegúrate de que esté importada. 
-           Si no la renombraste en el paso 1, déjala como estaba. 
-           Si la renombraste, actualiza aquí. */}
-        <Route path="/noticias" element={<Noticias />} />
-        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/informacion" element={<AboutPage />} />
+        {/* NOTA: Si tienes un archivo Informacion.jsx, renómbralo a "AboutPage.jsx" e impórtalo arriba igual que los otros */}
+
+        <Route path="/noticias" element={<EventsPage />} />
+        <Route path="/contacto" element={<ContactPage />} />
       </Routes>
 
-      <InfoBox />
-      <Footer />
+      <InfoCard />
+      <PageFooter />
     </BrowserRouter>
   );
 }
